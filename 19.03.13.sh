a7e19dd3de8ac92f01faf854902473015714eafa
Jonathan Whetsell <jrwhetse@gmail.com>
2020-10-01 09:47:08 -0400

    added sed for docker-ce for rhel system 7

--- 19.03.13.sh
+++ 19.03.13.sh
@@ -406,6 +406,11 @@ do_install() {
                                 fi
 				$sh_c "$pkg_manager install -y -q $pre_reqs"
 				$sh_c "$config_manager --add-repo $yum_repo"
+				
+				#jrw - added sed. replaces 7Server with 7 for rhel 7.5
+				#could be done so it doesn't do this for all rhel releases
+                                sed -i 's/$releasever/7/g' /etc/yum.repos.d/docker-ce.repo
+				
 				if [ "$CHANNEL" != "stable" ]; then
 					echo "Info: Enabling channel '$CHANNEL' for docker-ce repo"
 					$sh_c "$config_manager $enable_channel_flag docker-ce-$CHANNEL"
